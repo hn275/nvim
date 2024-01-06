20a31963c8c249f250597bbfb1c12690fd581b22
Hal <haln_01@proton.me>
2024-01-06 14:27:20 -0800

    feat: new color theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,6 +1,21 @@
-	return {
-		"lewpoly/sherbet.nvim",
-		config = function()
-			vim.cmd("colorscheme sherbet")
-		end,
-	}
+--[[
+return {
+	"lewpoly/sherbet.nvim",
+	config = function()
+		vim.cmd("colorscheme sherbet")
+	end,
+}
+--]]
+
+return {
+    "navarasu/onedark.nvim",
+    config = function ()
+        local colortheme = require("onedark")
+        colortheme.setup(
+            {
+                style = "deep"
+            }
+        )
+        colortheme.load()
+    end
+}
