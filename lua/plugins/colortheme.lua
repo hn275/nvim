49be8cbc459c14af8e250fb7e0f5ac139cc6446d
Hal <haln_01@proton.me>
2024-02-02 12:37:14 -0800

    chore: no clown theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -17,7 +17,6 @@ return {
     end,
 }
 
---]]
 
 return {
 	"olivercederborg/poimandres.nvim",
@@ -35,4 +34,26 @@ return {
 	init = function()
 		vim.cmd("colorscheme poimandres")
 	end,
+}
+
+--]]
+return {
+    "no-clown-fiesta/no-clown-fiesta.nvim",
+    config = function()
+        require("no-clown-fiesta").setup({
+            transparent = false, -- Enable this to disable the bg color
+            styles = {
+                -- You can set any of the style values specified for `:h nvim_set_hl`
+                comments = {},
+                keywords = {},
+                functions = {},
+                variables = {},
+                type = { bold = true },
+                lsp = { underline = true },
+            },
+        })
+    end,
+    init = function()
+        vim.cmd("colorscheme no-clown-fiesta")
+    end
 }
