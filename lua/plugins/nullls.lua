16645e376636196671b17387ffb7bd81ca1d9d21
Hal <haln_01@proton.me>
2024-05-28 10:18:29 -0700

    feat(lsp): clang format global - read from `~/.clang-format`

--- lua/plugins/nullls.lua
+++ lua/plugins/nullls.lua
@@ -1,25 +1,29 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local n = require("null-ls")
-        local code_actions = n.builtins.code_actions
-        local format = n.builtins.formatting
-        local hover = n.builtins.hover
-        local diagnostics = n.builtins.diagnostics
-        local completion = n.builtins.completion
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local n = require("null-ls")
+		local code_actions = n.builtins.code_actions
+		local format = n.builtins.formatting
+		local hover = n.builtins.hover
+		local diagnostics = n.builtins.diagnostics
+		local completion = n.builtins.completion
 
-        n.setup({
-            sources = {
-                format.stylua,
-                format.clang_format,
-                format.gofmt,
-                format.goimports,
-                format.prettier,
+		n.setup({
+			sources = {
+				format.stylua,
+				format.clang_format.with({
+					extra_args = {
+						"-style=file:" .. vim.fn.expand("~/.clang-format"),
+					},
+				}),
+				format.gofmt,
+				format.goimports,
+				format.prettier,
 
-                diagnostics.actionlint,
-                diagnostics.fish,
-                diagnostics.golangci_lint,
-            },
-        })
-    end,
+				diagnostics.actionlint,
+				diagnostics.fish,
+				diagnostics.golangci_lint,
+			},
+		})
+	end,
 }
