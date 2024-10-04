d8f0f901250b7fb3cd375b75890d6c221c1a66cf
Hal <haln_01@proton.me>
2024-10-03 23:59:04 -0700

    removed telescope cmd ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "jonarrien/telescope-cmdline.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim"},
 	config = function()
 		local has_telescope, telescope = pcall(require, "telescope")
 
@@ -62,12 +62,10 @@ return {
 			extensions = {},
 		})
 
-		require("telescope").load_extension("cmdline")
 
 		local t = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", t.find_files)
 		vim.keymap.set("n", "<leader>fg", t.live_grep)
 		vim.keymap.set("n", "<leader>fb", t.buffers)
-		vim.keymap.set("n", ":", "<cmd>Telescope cmdline<cr>")
 	end,
 }
