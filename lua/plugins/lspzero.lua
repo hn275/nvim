9dc2cab283635c7e817135de4263a49c56352c75
Hal <haln_01@proton.me>
2025-06-16 11:48:51 -0700

    refacotr lspzero config

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -1,3 +1,86 @@
+-- KEYBINDINGS
+local lsp = vim.lsp.buf
+local diag = vim.diagnostic
+
+local KEY_BINDS = {
+	{
+		mode = "n",
+		key = "K",
+		fn = lsp.hover,
+	},
+	{
+		mode = "n",
+		key = "gs",
+		fn = lsp.signature_help,
+	},
+	{
+		mode = "n",
+		key = "ga",
+		fn = lsp.code_action,
+	},
+	{
+		mode = "n",
+		key = "gd",
+		fn = lsp.definition,
+	},
+	{
+		mode = "n",
+		key = "gf",
+		fn = lsp.references,
+	},
+	{
+		mode = "n",
+		key = "ge",
+		fn = diag.open_float,
+	},
+	{
+		mode = "n",
+		key = "gn",
+		fn = function()
+			diag.jump({ count = 1, float = true })
+		end,
+	},
+	{
+		mode = "n",
+		key = "gp",
+		fn = function()
+			diag.jump({ count = -1, float = true })
+		end,
+	},
+	{
+		mode = "n",
+		key = "gD",
+		fn = diag.setqflist, -- open all diagnostics
+	},
+	{
+		mode = "n",
+		key = "<leader>rn",
+		fn = lsp.rename,
+	},
+}
+
+local function attach_key_bindings(lsp_zero)
+	lsp_zero.on_attach(function(_, bufnr)
+		for _, v in ipairs(KEY_BINDS) do
+			vim.keymap.set(v.mode, v.key, v.fn, { noremap = true, silent = true, buffer = bufnr })
+		end
+		lsp_zero.default_keymaps({ buffer = bufnr })
+	end)
+end
+
+-- This is a list of LSP servers to be installed and configured.
+local SERVERS = {
+	"bashls",
+	"clangd",
+	"pyright",
+	"rust_analyzer",
+	"gopls",
+	"ts_ls",
+	"tinymist",
+	"lua_ls",
+	"emmet_language_server",
+}
+
 return {
 	"VonHeikemen/lsp-zero.nvim",
 	dependencies = {
@@ -9,96 +92,16 @@ return {
 	config = function()
 		local lsp_zero = require("lsp-zero")
 
-		local servers = {
-			"bashls",
-			"clangd",
-			"pyright",
-			"rust_analyzer",
-			"gopls",
-			"ts_ls",
-			"tinymist",
-			"lua_ls",
-			"emmet_language_server",
-		}
-
 		require("mason").setup()
 		require("mason-lspconfig").setup({
-			ensure_installed = servers,
+			ensure_installed = SERVERS,
 			handlers = {
 				lsp_zero.default_setup,
 			},
+			automatic_enable = true,
 		})
 
-		for _, server in ipairs(servers) do
-			vim.lsp.enable(server)
-		end
-
-		-- keybindings
-		local lsp = vim.lsp.buf
-		local diag = vim.diagnostic
-		local keybindings = {
-			{
-				mode = "n",
-				key = "K",
-				fn = lsp.hover,
-			},
-			{
-				mode = "n",
-				key = "gs",
-				fn = lsp.signature_help,
-			},
-			{
-				mode = "n",
-				key = "ga",
-				fn = lsp.code_action,
-			},
-			{
-				mode = "n",
-				key = "gd",
-				fn = lsp.definition,
-			},
-			{
-				mode = "n",
-				key = "gf",
-				fn = lsp.references,
-			},
-			{
-				mode = "n",
-				key = "ge",
-				fn = diag.open_float,
-			},
-			{
-				mode = "n",
-				key = "gn",
-				fn = function()
-					diag.jump({ count = 1, float = true })
-				end,
-			},
-			{
-				mode = "n",
-				key = "gp",
-				fn = function()
-					diag.jump({ count = -1, float = true })
-				end,
-			},
-			{
-				mode = "n",
-				key = "gD",
-				fn = diag.setqflist, -- open all diagnostics
-			},
-			{
-				mode = "n",
-				key = "<leader>rn",
-				fn = lsp.rename,
-			},
-		}
-
-		lsp_zero.on_attach(function(_, bufnr)
-			for _, v in ipairs(keybindings) do
-				vim.keymap.set(v.mode, v.key, v.fn, { noremap = true, silent = true, buffer = bufnr })
-			end
-			lsp_zero.default_keymaps({ buffer = bufnr })
-		end)
+		attach_key_bindings(lsp_zero)
 
 		-- lua lsp has to be configed this way to get global vim api
 		vim.lsp.config("lua_ls", {
